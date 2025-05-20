9ee66fc466016048a1e8bca0f993eff3f0dcdfdd
shivang991 <32.shivangdubey@gmail.com>
2025-05-21 03:01:36 +0530

    feat: add restart button

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -7,7 +7,12 @@ const lvlMtFuji = preload("res://scenes/level_mt_fuji.tscn")
 
 func start_lvl(level: PackedScene):
 	var lvl = level.instantiate()
-	remove_child(get_node("SplashScreen"))
+	lvl.restart_pressed.connect(start_lvl.bind(level))
+	
+	var children = get_children()
+	for c in children:
+		remove_child(c)
+		
 	add_child(lvl)
 
 var start_lvl_mt_fuji := start_lvl.bind(lvlMtFuji)
--- scenes/level_mt_fuji.gd
+++ scenes/level_mt_fuji.gd
@@ -3,18 +3,21 @@ extends Node3D
 var is_lerping_go_screen := false
 var go_screen_alpha := 0
 
+signal restart_pressed
+
 # Hide the game over screen by default
 func _ready():
 	$GameOverScreen.visible = false
+	$GameOverScreen/ButtonContainer/RestartButton.visible = true
 
 # Handle player death
 func _on_death_region_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("player"):
 		return
-	print("death region entered")
 	$GameOverScreen.visible = true
 	is_lerping_go_screen = true
 	$GameOverScreen/Background.color = Color.from_rgba8(0, 0, 0, 0)
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 func _physics_process(delta: float) -> void:
@@ -26,5 +29,11 @@ func _physics_process(delta: float) -> void:
 		if go_screen_alpha >= 255:
 			go_screen_alpha = 255
 			is_lerping_go_screen = false
+			$GameOverScreen/ButtonContainer/RestartButton.visible = false
 
 		$GameOverScreen/Background.color = Color.from_rgba8(0, 0, 0, go_screen_alpha)
+	
+
+
+func _on_restart_button_pressed():
+	restart_pressed.emit()
